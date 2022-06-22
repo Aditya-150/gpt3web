1f0d13fe5fab471083d7256dd48a70e77909f651
Aditya <adityaprasadmohanty150@gmail.com>
2022-06-22 08:55:13 +0530

    Project done!!

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,25 +1,51 @@
 import React from "react";
-import "./footer.css";
 import gpt3Logo from "../../assets/logo.svg";
+import "./footer.css";
 
-const Footer = () => {
-  return (
-    <div className="gpt3__footer section__padding">
-      <div className="gpt3__footer-heading">
-        <h1 className="gradient__text">
-          Do you want to step in to the future before others
-        </h1>
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">
+        Do you want to step in to the future before others
+      </h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links__logo">
+        <img src={gpt3Logo} alt="gpt3__logo" />
+        <p>
+          Nobody else, <br /> All Rights Reserved
+        </p>
       </div>
-      <div className="gpt3__footer-btn">
-        <p>Request Early Access</p>
+      <div className="gpt3__footer-links__div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
       </div>
-      <div className="gpt3__footer-links">
-        <div className="gpt3__footer-links__logo">
-          <img src={gpt3Logo} alt="Logo"/>
-        </div>
+      <div className="gpt3__footer-links__div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links__div">
+        <h4>Get in touch</h4>
+        <p>Nobody else</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
       </div>
     </div>
-  );
-}
 
-export default Footer
+    <div className="gpt3__footer-copyright">
+      <p>@2021 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
